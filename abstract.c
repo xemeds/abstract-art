d97c556f370412135b873926d24f5bcd92932601
xemeds <xemeds@protonmail.com>
2021-08-31 13:54:39 +0300

    Added a way to choose custom background color

--- abstract.c
+++ abstract.c
@@ -5,21 +5,17 @@
 
 #define WIDTH 1600
 #define HEIGHT 1600
+#define PADDING_MARGIN 16
 #define FILENAME "image.png"
 
 int main() {
-    Pixel_t image[WIDTH][HEIGHT];
-
-    for (int h = 0; h < HEIGHT; h++) {
-        for (int w = 0; w < WIDTH; w++) {
-            color(WIDTH, HEIGHT, image, h, w, 255, 255, 255);
-        }
-    }
-
     int amount_of_dots_lower = 3000;
     int amount_of_dots_higher = 5000;
     int dot_radius_lower = 1;
     int dot_radius_higher = 25;
+    int bg_r = 255;
+    int bg_g = 255;
+    int bg_b = 255;
 
     char option;
     do {
@@ -32,6 +28,13 @@ int main() {
     } while (option != 'Y' && option != 'y' && option != 'N' && option != 'n');
 
     if (option == 'N' || option == 'n') {
+        printf("Backgound color red value 0 - 255 (default %i): ", bg_r);
+        scanf("%d", &bg_r);
+        printf("Backgound color green value 0 - 255 (default %i): ", bg_g);
+        scanf("%d", &bg_g);
+        printf("Backgound color blue value 0 - 255 (default %i): ", bg_b);
+        scanf("%d", &bg_b);
+
         printf("Lower bound for the amount of dots (default %i): ", amount_of_dots_lower);
         scanf("%d", &amount_of_dots_lower);
         printf("Higher bound for the amount of dots (default %i): ", amount_of_dots_higher);
@@ -42,12 +45,20 @@ int main() {
         scanf("%d", &dot_radius_higher);
     }
 
+    Pixel_t image[WIDTH][HEIGHT];
+
+    for (int h = 0; h < HEIGHT; h++) {
+        for (int w = 0; w < WIDTH; w++) {
+            color(WIDTH, HEIGHT, image, h, w, bg_r, bg_g, bg_b);
+        }
+    }
+
     time_t t;
     srand((unsigned) time(&t));
-
-    int padding_x = (WIDTH / 16);
-    int padding_y = (HEIGHT / 16);
     // (rand() % (upper - lower + 1)) + lower
+
+    int padding_x = (WIDTH / PADDING_MARGIN);
+    int padding_y = (HEIGHT / PADDING_MARGIN);
     for (int i = 0; i < (rand() % ((amount_of_dots_higher + 1) - amount_of_dots_lower + 1)) + amount_of_dots_lower; i++) {
         int x = (rand() % ((WIDTH - padding_x + 1) - padding_x + 1)) + padding_x;
         int y = (rand() % ((HEIGHT - padding_y + 1) - padding_y + 1)) + padding_y;
